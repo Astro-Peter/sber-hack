4dfa34af5fb3ddf29a17971ade5ed337619032f9
Astro-Peter <Vanst63@yandex.ru>
2024-04-24 17:41:28 +0300

    changed database methods

--- DatabaseMethods/DatabaseMethodsAbstract.py
+++ DatabaseMethods/DatabaseMethodsAbstract.py
@@ -1,5 +1,9 @@
+import datetime
+
+
 class DatabaseMethodsAbstract:
     # gets the dictionary with themes as keys and their scores as numerical values
     def input(self,
-              values: dict):
+              values: dict,
+              date: datetime.date):
         pass
