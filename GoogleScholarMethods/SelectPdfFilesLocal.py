9b7fcf68e370d7ac0385fa3a15948baf6d7fdf99
Astro-Peter <Vanst63@yandex.ru>
2024-04-25 21:34:26 +0300

    fixed up some things and added stats.csv

--- GoogleScholarMethods/SelectPdfFilesLocal.py
+++ GoogleScholarMethods/SelectPdfFilesLocal.py
@@ -8,16 +8,5 @@ class SelectPdfFilesLocal(SelectFilesAbstract):
     def getNewPublications(self,
                            from_date: date,
                            to_date: date) -> pd.DataFrame:
-        data = {"date": [], "cities": [], "url": []}
-
-        with open("stats.csv", "rb") as csv:
-            for line in csv.readlines():
-                words = line.__str__().split(";")
-                submitted_date = date.fromisoformat(words[0][2:])
-                if submitted_date < from_date or submitted_date > to_date:
-                    continue
-                data["date"].append(submitted_date)
-                data["cities"].append(int(words[1]))
-                data["url"].append(words[2])
-
-        return pd.DataFrame(data)
+        data = pd.read_csv("stats.csv")
+        return data[(data["date"] <= to_date) & (data["date"] >= from_date)]
