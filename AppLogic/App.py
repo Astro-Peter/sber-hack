b51687aa337b4ea65e48b1c97935889c7cb2fa0a
Astro-Peter <Vanst63@yandex.ru>
2024-04-25 23:52:20 +0300

    add: business logic

--- AppLogic/App.py
+++ AppLogic/App.py
@@ -1,2 +1,50 @@
+import datetime
+
+import pandas as pd
+
+import DatabaseMethods.DatabaseMethodsAbstract as db
+import GigaChainMethods.GigaChainMethodsAbstract as gch
+import ArticlesMethods.SelectFilesAbstract as gsm
+import ArticlesMethods.GetRawText as rtx
+
+
 class App:
-    pass
+    database: db.DatabaseMethodsAbstract
+    gigachain: gch.GigaChainMethodsAbstract
+    file_getter: gsm.SelectFilesAbstract
+    text_getter: rtx.GetRawText
+    curr_data: pd.DataFrame
+
+    def __init__(self,
+                 database: db.DatabaseMethodsAbstract,
+                 gigachain: gch.GigaChainMethodsAbstract,
+                 file_getter: gsm.SelectFilesAbstract,
+                 text_getter: rtx.GetRawText):
+        self.database = database
+        self.gigachain = gigachain
+        self.text_getter = text_getter
+        self.file_getter = file_getter
+
+    def get_topics(self) -> pd.DataFrame:
+        return self.database.get_all_topics()
+
+    def get_all_data_from_year(self, year: int) -> pd.DataFrame:
+        self.curr_data = self.database.extract_topics_by_year(year)
+        return self.curr_data
+
+    def get_all_data_from_month_year(self, year: int, month: int) -> pd.DataFrame:
+        self.curr_data = self.database.extract_topics_by_month_year(year, month)
+        return self.curr_data
+
+    def get_all_data_from_name(self, topic: str) -> pd.DataFrame:
+        self.curr_data = self.database.extract_topics_by_name(topic)
+        return self.curr_data
+
+    def add_data(self, from_date: datetime.date, to_date: datetime.date):
+        articles = self.file_getter.get_new_publications(from_date, to_date)
+        for index, article in articles.iterrows():
+            text = self.text_getter.get_text(article["link"])
+            data = self.gigachain.get_themes(text)
+            print(data)
+            for key in data:
+                self.database.increment_topic_rate(key, data[key] * article["citations"], article["date"])
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-def func() -> int:
-    return 1.2
+import datetime
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import AppLogic.App
+import DatabaseMethods.DatabaseMethodsImpl
+import ArticlesMethods.SelectPdfFilesLocal
+import ArticlesMethods.GetRawTextFromURL
+from GigaChainMethods import GigaChainMethodsImpl
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(func())
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    conn_string = "dbname=sber-hack-database user=admin password=admin host=localhost port=2432"
+    database = DatabaseMethods.DatabaseMethodsImpl.DatabaseMethods(conn_string)
+    gigachat_methods = GigaChainMethodsImpl.GigaChainMethodsImpl("NTlkY2MyZmItM2Q4ZC00ZWMzLWE2NjAtNTI3MzZhOTk2ZjQzOjVhZGJiZDQxLTc0YjAtNDQxNi04YjAzLTUxZDVmYTY4NTkwNw==")
+    raw_text = ArticlesMethods.GetRawTextFromURL.GetRawTextFromURL()
+    files_select = ArticlesMethods.SelectPdfFilesLocal.SelectPdfFilesLocal()
+    app = AppLogic.App.App(database, gigachat_methods, files_select, raw_text)
+    from_date = datetime.date(2020, 1, 1)
+    to_date = datetime.date(2021, 12, 31)
+    app.add_data(from_date, to_date)
